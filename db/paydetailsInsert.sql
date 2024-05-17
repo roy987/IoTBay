7e021cc9927c5365d0fb35b27490ce2d25025cfb
EnochPei <109504082+EnochPei@users.noreply.github.com>
2024-05-17 12:22:39 +1000

    Create paydetailsInsert.sql

---
+++ db/paydetailsInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO payment (cardNumber, cardName, expiryDate, cvv, email) VALUES
+('1237539409126418', 'real name1', '06/25', '123', 'john123@mail.com'),
+('7241182343789102', 'real name2', '07/25', '333', 'john123@mail.com');
