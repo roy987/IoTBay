2fa39af1da7f20dcd63a31c2fa4ebaaf79d738ef
EnochPei <109504082+EnochPei@users.noreply.github.com>
2024-05-17 12:22:03 +1000

    Create paymentInsert.sql

---
+++ db/paymentInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO payment (paymentID, cardNumber, cardName, expiryDate, cvv, orderID, email) VALUES
+('PAY001', '1237539409126418', 'real name1, '06/25', '123', '1001', 'john123@mail.com'),
+('PAY002', '7241182343789102', ‘real  name2', '07/25', '333', '2222', 'john123@mail.com');
